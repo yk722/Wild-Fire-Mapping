a4847c5f9137c950c6b8f567ab2c2c3a91720ba7
Jen <yu204830@hotmail.com>
2024-10-05 16:59:42 -0700

    setting the web page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
@@ -12,6 +12,16 @@ def run_sim():
     # add button function
     print("Button clicked")
     return redirect(url_for('home'))  # Redirect back to home after processing
+
+# button for finding the shortest path
+@app.route('/search',methods=['POST'])
+def search():
+    # add button function
+    search_term = request.form.get('search_term')
+    print(f"Search term: {search_term}")
+    
+    return redirect(url_for('home'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
