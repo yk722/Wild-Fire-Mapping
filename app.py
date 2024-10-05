fc14fb071bfdfd36d0730f47f86f0aa29819bcc7
Jen <yu204830@hotmail.com>
2024-10-05 11:04:15 -0700

    added tmeplates folder

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def home():
+    return render_template('index.html', name="Wild-Fire-Mapping")
 
 if __name__ == "__main__":
     app.run(debug=True)
