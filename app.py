02f30050245159324e7aae1dde698ffbc7e1892c
Jen <yu204830@hotmail.com>
2024-10-05 12:25:22 -0700

    setting up html and css

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html', name="Wild-Fire-Mapping")
+
+# button for finding the shortest path
+@app.route('/run_sim', methods=['POST'])
+def run_sim():
+    # add button function
+    print("Button clicked")
+    return redirect(url_for('home'))  # Redirect back to home after processing
 
 if __name__ == "__main__":
     app.run(debug=True)
