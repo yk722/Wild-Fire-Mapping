21a2f77a3cc5a970c550a4efa9f36fb8ab78d3db
Jen <yu204830@hotmail.com>
2024-10-05 20:47:27 -0700

    added map

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, url_for, redirect, request
+import folium
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', name="Wild-Fire-Mapping")
+    # Create a map centered at a specific location
+    map = folium.Map(
+        location=[49.26218662575472, -123.24927174424609],
+        titles='Stamen Terrain',
+        zoom_start=12,
+    )
+    map.get_root().width = "1000px"
+    map.get_root().height = "400px"
+    iframe = map.get_root()._repr_html_()
+
+    # # Save the map to an HTML file
+    # map_path = 'static/map.html'
+    # m.save(map_path)
+
+    return render_template('index.html', map_file='map.html', iframe=iframe)
 
 # button for finding the shortest path
 @app.route('/run_sim', methods=['POST'])
